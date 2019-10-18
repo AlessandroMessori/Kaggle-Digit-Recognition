9546cc719fdf8eec7a554d642022f242fc6011f8
AlessandroMessori <messori.alessandro.98@gmail.com>
2019-10-19 00:34:32 +0200

    updated cnn structure,0.98085 accuracy,position 1438

--- src/cnn.py
+++ src/cnn.py
@@ -15,13 +15,13 @@ class DigitCNN:
         self.cnn.add(Conv2D(64, kernel_size=3, activation="relu", input_shape=(28, 28, 1)))
         self.cnn.add(Conv2D(32, kernel_size=3, activation="relu"))
         self.cnn.add(Flatten())
-        #self.cnn.add(Dense(16, activation="relu"))
+        self.cnn.add(Dense(32, activation="relu"))
         self.cnn.add(Dropout(0.2))
         self.cnn.add(Dense(10, activation="softmax"))
 
     def train(self):
         self.cnn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-        self.cnn.fit(self.x, self.y, epochs=5)
+        self.cnn.fit(self.x, self.y, epochs=10)
 
     def predict(self, to_pred):
         preds = self.cnn.predict(to_pred)
