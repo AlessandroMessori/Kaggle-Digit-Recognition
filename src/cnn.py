0e258119c5d83661a06e094e0c8c949a84127d1e
AlessandroMessori <messori.alessandro.98@gmail.com>
2019-10-23 21:38:10 +0200

    update

--- src/cnn.py
+++ src/cnn.py
@@ -1,6 +1,6 @@
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, BatchNormalization
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization
 from tensorflow.keras.utils import to_categorical
 
 
@@ -14,18 +14,20 @@ class DigitCNN:
     def build(self):
         self.cnn.add(Conv2D(64, kernel_size=3, activation="relu", input_shape=(28, 28, 1)))
         self.cnn.add(Conv2D(32, kernel_size=3, activation="relu"))
+        self.cnn.add(MaxPooling2D(pool_size=(2, 2)))
+        self.cnn.add(Dropout(0.28))
         self.cnn.add(Flatten())
         self.cnn.add(Dense(64, activation="relu"))
         self.cnn.add(BatchNormalization())
         self.cnn.add(Dropout(0.3))
         self.cnn.add(Dense(128, activation="relu"))
         self.cnn.add(BatchNormalization())
-        self.cnn.add(Dropout(0.3))
+        self.cnn.add(Dropout(0.32))
         self.cnn.add(Dense(10, activation="softmax"))
 
     def train(self):
         self.cnn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-        self.cnn.fit(self.x, self.y, epochs=7)
+        self.cnn.fit(self.x, self.y, epochs=20)
 
     def predict(self, to_pred):
         preds = self.cnn.predict(to_pred)
